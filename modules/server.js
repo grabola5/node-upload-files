8181d39f6e9ab0216e25a71bf60fd11469901901
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-08-19 11:51:17 +0200

    Add code to handle requests

--- modules/server.js
+++ modules/server.js
@@ -1,13 +1,28 @@
 var http = require('http');
 var colors = require('colors');
 
+var handlers = require('./handlers');
+
 function start() {
 	function onRequest(request, response) {
-		console.log("Odebrano zapytanie.");
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Pierwsze koty za płoty");
-		response.end();
-	}
+		console.log("Odebrano zapytanie." .green);
+		console.log("Zapytanie " + request.url + "odebrane.");
+
+		response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+
+		switch (request.url) {
+			case '/':
+			case '/start':
+				handlers.welcome(request, response); //wywołanie tej samej funkcji w dwóch przypadkach
+				break;
+			case '/upload':
+				handlers.upload(request, response);
+				break;
+			default: //jeśli zapytanie nie zostanie rozpoznane
+				handlers.error(request, response);
+			}
+		}
+
 	http.createServer(onRequest).listen(9000);
 
 	console.log("Uruchomiono serwer" .green);
